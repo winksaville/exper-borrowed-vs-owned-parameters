49ae44694aab95ed2f1537cd64972a895e5510e1
Wink Saville <wink@saville.com>
2022-12-01 09:20:55 -0800

    feat: Simplify
    Remove summation loop just return first value

--- src/lib.rs
+++ src/lib.rs
@@ -8,36 +8,28 @@ struct Message {
 
 #[inline(never)]
 fn message_borrowed(msg: &Message) -> u32 {
-    let mut sum = 0u32;
-    for i in 0..msg.v.len() {
-        sum += msg.v[i] as u32
-    }
-    sum
+    msg.v[0] as u32
 }
 
 #[inline(never)]
 fn message_owned(msg: Message) -> (u32, Message) {
-    let mut sum = 0u32;
-    for i in 0..msg.v.len() {
-        sum += msg.v[i] as u32
-    }
-    (sum, msg)
+    (msg.v[0] as u32, msg)
 }
 
 #[inline(never)]
 pub fn invoke_message_borrowed() {
-    let msg = Message { v: vec![0, 1, 2, 3] };
+    let msg = Message { v: vec![2] };
     let r1 = black_box(message_borrowed(&msg));
     let r2 = black_box(message_borrowed(&msg));
-    assert!(r1 == 6);
+    assert!(r1 == 2);
     assert!(r1 == r2);
 }
 
 #[inline(never)]
 pub fn invoke_message_owned() {
-    let msg = Message { v: vec![0, 1, 2, 3] };
+    let msg = Message { v: vec![3] };
     let (r1, msg) = black_box(message_owned(msg));
     let (r2, _msg) = black_box(message_owned(msg));
-    assert!(r1 == 6);
+    assert!(r1 == 3);
     assert!(r1 == r2);
 }
