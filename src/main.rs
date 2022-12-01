e3502155141dfe029972f798254d9c950304dfbd
Wink Saville <wink@saville.com>
2022-12-01 11:57:25 -0800

    refactor: cargo fmt changes

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,7 @@
 use exper_borrowed_vs_owned_parameters::{
-    invoke_message_borrowed,
-    invoke_message_borrowed_idx_loop,
-    invoke_message_borrowed_iter_loop,
-    invoke_message_owned,
-    invoke_message_owned_idx_loop,
-    invoke_message_owned_iter_loop,
+    invoke_message_borrowed, invoke_message_borrowed_idx_loop, invoke_message_borrowed_iter_loop,
+    invoke_message_owned, invoke_message_owned_idx_loop, invoke_message_owned_iter_loop,
 };
-
 
 fn main() {
     invoke_message_borrowed();
