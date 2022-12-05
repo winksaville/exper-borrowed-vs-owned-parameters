7e7ebd43a2a37b8e271926a9f0f5f273d900c506
Wink Saville <wink@saville.com>
2022-12-05 10:10:38 -0800

    feat: Add boxed parameters
    These are "owned" and thus also part of the return value.
    
    TODO, update README.md

--- benches/iai_bench.rs
+++ benches/iai_bench.rs
@@ -1,4 +1,6 @@
 use exper_borrowed_vs_owned_parameters::{
+    invoke_boxed_msgmf, invoke_boxed_msgmf_default, invoke_boxed_msgnf, invoke_boxed_msgnf_default,
+    invoke_boxed_msgof, invoke_boxed_msgof_default, invoke_boxed_msgsf, invoke_boxed_msgsf_default,
     invoke_msgmf_borrowed, invoke_msgmf_default, invoke_msgmf_owned, invoke_msgnf_borrowed,
     invoke_msgnf_default, invoke_msgnf_owned, invoke_msgof_borrowed, invoke_msgof_default,
     invoke_msgof_owned, invoke_msgsf_borrowed, invoke_msgsf_default, invoke_msgsf_owned,
@@ -9,6 +11,10 @@ iai::main!(
     invoke_msgof_default,
     invoke_msgsf_default,
     invoke_msgmf_default,
+    invoke_boxed_msgnf_default,
+    invoke_boxed_msgof_default,
+    invoke_boxed_msgsf_default,
+    invoke_boxed_msgmf_default,
     invoke_msgnf_borrowed,
     invoke_msgnf_owned,
     invoke_msgof_borrowed,
@@ -17,4 +23,8 @@ iai::main!(
     invoke_msgsf_owned,
     invoke_msgmf_borrowed,
     invoke_msgmf_owned,
+    invoke_boxed_msgnf,
+    invoke_boxed_msgof,
+    invoke_boxed_msgsf,
+    invoke_boxed_msgmf,
 );
--- gen_asm.sh
+++ gen_asm.sh
@@ -10,16 +10,24 @@ gen_asm () {
     cargo asm --rust --lib "exper_borrowed_vs_owned_parameters::$1" > asm/$1.txt
 }
 
-#gen_asm "invoke_msgnf_default" # optimized away?
+#gen_asm "invoke_msgnf_default" # optimized away
 gen_asm "invoke_msgof_default"
 gen_asm "invoke_msgsf_default"
 gen_asm "invoke_msgmf_default"
-#gen_asm "invoke_msgnf_borrowed" # optimized away?
-gen_asm "invoke_msgnf_owned"     # This is actually just a `ret`
+#gen_asm "invoke_boxed_msgnf_default" # optimized away
+gen_asm "invoke_boxed_msgof_default"
+gen_asm "invoke_boxed_msgsf_default"
+gen_asm "invoke_boxed_msgmf_default"
+#gen_asm "invoke_msgnf_borrowed" # optimized away
+gen_asm "invoke_msgnf_owned"     # This is just a `ret`
 gen_asm "invoke_msgof_borrowed"
 gen_asm "invoke_msgof_owned"
 gen_asm "invoke_msgsf_borrowed"
 gen_asm "invoke_msgsf_owned"
 gen_asm "invoke_msgmf_borrowed"
 gen_asm "invoke_msgmf_owned"
+gen_asm "invoke_boxed_msgnf"     # This is just a `ret`
+gen_asm "invoke_boxed_msgof"
+gen_asm "invoke_boxed_msgsf"
+gen_asm "invoke_boxed_msgmf"
 
